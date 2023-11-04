e3b09a0be9dc128c775ab0dc191f6ea7d80fb55e
Kamran Ansari <ranmerc@outlook.com>
2023-11-04 17:23:54 +0530

    [7] added first iteration of dictionary search

---
+++ 7_maps/dictionary_test.go
@@ -0,0 +1,20 @@
+package dictionary
+
+import "testing"
+
+func TestDictionary(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
