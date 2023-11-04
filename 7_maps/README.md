7d5b3849c2127fa8093d0344eef9068da5101d00
Kamran Ansari <ranmerc@outlook.com>
2023-11-04 18:34:36 +0530

    [7] added Dictionary type, handle unknown words

--- 7_maps/README.md
+++ 7_maps/README.md
@@ -1,1 +1,19 @@
 # Notes on Maps
+
+Declaring a Map is somewhat similar to an array. Except, it starts with the map keyword and requires two types. The first is the key type, which is written inside the []. The second is the value type, which goes right after the [].
+
+```go
+  map[string][string]
+```
+
+Errors can be converted to a string with the `.Error()` method.
+
+We can return multiple items from a function
+
+```go
+func (d Dictionary) Search(word string) (string, error) {
+  return d[word], nil
+}
+```
+
+A map lookup can return 2 values. The second value is a boolean which indicates if the key was found successfully.
--- 7_maps/dictionary_test.go
+++ 7_maps/dictionary_test.go
@@ -3,15 +3,35 @@ package dictionary
 import "testing"
 
 func TestDictionary(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		if got == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertError(t, got, ErrNotFound)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
 	t.Helper()
 
 	if got != want {
